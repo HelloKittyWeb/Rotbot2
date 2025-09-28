f54e27b0df52fbf230836ed79e12d5c16c2e30a0
HelloKittyWeb <kaaatka0903@gmail.com>
2025-09-28 17:46:56 +0300

    config.py

---
+++ config.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+import os
+
+# system
+debug = False
+
+# telegram bot
+bot_token = '8348845652:AAHLloipxWBwuuffx9Rnz0Y57fh1NxA6bFM'
+bot_admins = [837681107]
+accept_topic_ids = [23452,]
